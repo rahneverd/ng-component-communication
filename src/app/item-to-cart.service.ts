5a67fcbfa1a2515ff98209ca4f643f5327441389
rahneverd <rahneverd@gmail.com>
2022-12-26 17:45:47 +0500

    EventEmitter implemented

--- src/app/item-to-cart.service.ts
+++ src/app/item-to-cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 // import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
@@ -9,13 +9,15 @@ export class ItemToCartService {
 
   // item = new BehaviorSubject<string>('')
   // finalItem = this.item.asObservable()
-  item = new Subject<string>
+  // item = new Subject<string>
+  item = new EventEmitter<string>()
 
 
   constructor() {}
 
   changeItem(itemRcvd) {
     // this.item.next(itemRcvd)
-    this.item.next(itemRcvd)
+    // this.item.next(itemRcvd)
+    this.item.emit(itemRcvd)
   }
 }
